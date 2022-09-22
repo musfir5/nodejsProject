f711ade2e8481a28f23c892e766ebe5d0cbf2da8
musfir <musfirmohammed555@gmail.com>
2022-09-22 10:57:06 +0530

    first database connection

---
+++ productHelpers/prod-helpers.js
@@ -0,0 +1,27 @@
+const { resolve, reject } = require('promise');
+var db = require('../config/connection')
+var collection = require('../config/collections')
+
+module.exports = {
+
+    addProduct: (product, callback) => {
+
+        console.log(product);
+        db.get().collection('product').insertOne(product).then((data) => {
+
+
+            //console.log("id=====" + data)
+            callback(data.insertedId)
+
+        })
+    },
+    getAllProducts: () => {
+
+        return new Promise(async(resolve, reject) => {
+
+            let products = await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
+        })
+
+    }
+}
---
+++ routes/admin.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var prodHelpers = require('../productHelpers/prod-helpers');
+var router = express.Router();
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+    prodHelpers.getAllProducts().then((products) => {
+        console.log(products)
+        res.render('admin/view_products', { admin: true, products })
+    })
+
+
+
+});
+
+router.get('/add-product', (req, res) => {
+
+    res.render('admin/add-product')
+
+})
+
+router.post('/add-product', (req, res) => {
+
+    console.log(req.body);
+    console.log(req.files.Images);
+
+    prodHelpers.addProduct(req.body, (id) => {
+        let image = req.files.Images
+        console.log(id)
+        image.mv('./public/product-images/' + id + '.jpg', (err, done) => {
+
+            if (!err) {
+                res.render('admin/add-product');
+            } else {
+                console.log(err);
+            }
+        })
+
+
+    })
+})
+
+module.exports = router;
